4fba6a60503bce05493a373df506ef2e8f5b6e31
v-yarotsky <vladimir.yarotsky@gmail.com>
2012-05-05 01:59:19 +0300

    Simple help

--- bin/medo.rb
+++ bin/medo.rb
@@ -2,13 +2,21 @@
 
 require_relative '../config/environment'
 
+def show_help
+  puts "List of available commands: #{COMMANDS.join(", ")}"
+end
+
 begin
   COMMANDS = %W(new done list note clear delete).freeze
 
   cmd = ARGV.shift or raise "No command given!"
-  raise "No such command: #{cmd}" unless COMMANDS.include?(cmd)
-
-  require_relative "medo-#{cmd}.rb"
+  if cmd == "-h"
+    show_help
+  else
+    raise "No such command: #{cmd}" unless COMMANDS.include?(cmd)
+    require_relative "medo-#{cmd}.rb"
+  end
 rescue Exception => e
   puts e.message
 end
+
